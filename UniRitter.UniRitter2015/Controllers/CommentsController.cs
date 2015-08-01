dff72bcbf7693906472521027e7b96b6cbee4ee8
ajmatos <antonio.jose.matos@gmail.com>
2015-08-01 05:38:29 -0700

    +1

--- UniRitter.UniRitter2015/Controllers/CommentsController.cs
+++ UniRitter.UniRitter2015/Controllers/CommentsController.cs
@@ -1,64 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 using UniRitter.UniRitter2015.Models;
 using UniRitter.UniRitter2015.Services;
 
 namespace UniRitter.UniRitter2015.Controllers
 {
-    public class CommentsController : ApiController
+
+    public class CommentsController : BaseController<PostModel>
     {
         private readonly IRepository<CommentModel> _repo;
 
         public CommentsController(IRepository<CommentModel> repo)
         {
-            this._repo = repo;
-        }
-
-        // GET: api/Comment
-        public IHttpActionResult Get()
-        {
-            return Json(_repo.GetAll());
-        }
-
-        // GET: api/Comment/5
-        public IHttpActionResult Get(Guid id)
-        {
-            var data = _repo.GetById(id);
-            if (data != null) {
-                return Json(data);
-            } else {
-              return NotFound();
-            }
-        }
-
-        // POST: api/Comment
-        public IHttpActionResult Post([FromBody]CommentModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var data = _repo.Add(model);
-                return Json(data);
-            } else {
-                return BadRequest(ModelState);
-            }
-        }
-
-        // PUT: api/Comment/5
-        public IHttpActionResult Put(Guid id, [FromBody]CommentModel model)
-        {
-            var data = _repo.Update(id, model);
-            return Json(model);
-        }
-
-        // DELETE: api/Comment/5
-        public IHttpActionResult Delete(Guid id)
-        {
-            _repo.Delete(id);
-            return StatusCode(HttpStatusCode.NoContent);
+            _repo = repo;
         }
     }
 }
